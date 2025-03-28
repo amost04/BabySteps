32bf62680b73d98ba2fee0b1268ce510d6b9427e
amost04 <a373767@uach.mx>
2025-03-27 19:23:24 -0700

    Modificación en auth_app.js

--- FB/auth_app.js
+++ FB/auth_app.js
@@ -1,7 +1,10 @@
 import app from "./Firebase";
-import {getAuth, crateUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, delateUser} from "firebase/auth";
+import {getAuth, crateUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, delateUser, createUserWithEmailAndPassword} from "firebase/auth";
 const authInstance = getAuth(app);
 
+async function singUp (correo, contraseña) {
+    createUserWithEmailAndPassword(authInstance, correo, contraseña)
+}
 export{
-
+    singUp
 }
